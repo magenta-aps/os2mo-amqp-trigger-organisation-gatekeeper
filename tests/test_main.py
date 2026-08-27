9e42efebff8741a22a2e6f3ec5e123324181bde1
Lorenzo Panieri <lp@magenta.dk>
2026-08-27 14:32:33 +0200

    test: [#70974] drop tests for endpoints handled by FastRAMQPI

--- tests/test_main.py
+++ tests/test_main.py
@@ -17,13 +17,10 @@ from uuid import UUID
 from uuid import uuid4
 
 import pytest
-from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
-from orggatekeeper.config import Settings
 from orggatekeeper.main import build_information
-from orggatekeeper.main import construct_clients
 from orggatekeeper.main import create_app
 from orggatekeeper.main import update_build_information
 from tests import DEFAULT_AMQP_URL
@@ -103,28 +100,6 @@ def test_client(
     yield test_client_builder()
 
 
-async def test_root_endpoint(test_client: TestClient) -> None:
-    """Test the root endpoint on our app."""
-    response = test_client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"name": "orggatekeeper"}
-
-
-async def test_metrics_endpoint(test_client_builder: Callable[..., TestClient]) -> None:
-    """Test the metrics endpoint on our app."""
-    test_client = test_client_builder(
-        default_args=False,
-        fastramqpi={
-            "client_secret": "hunter2",
-            "client_id": "orggatekeeper_test",
-            "amqp": {"url": DEFAULT_AMQP_URL},
-        },
-    )
-    response = test_client.get("/metrics")
-    assert response.status_code == 200
-    assert "# TYPE build_information_info gauge" in response.text
-
-
 @patch("orggatekeeper.api.update_line_management", return_value=AsyncMock())
 async def test_trigger_uuid_endpoint(
     update_line_management_mock: AsyncMock,
@@ -139,183 +114,6 @@ async def test_trigger_uuid_endpoint(
     assert update_line_management_mock.mock_calls == [
         call(uuid=UUID("0a9d7211-16a1-47e1-82da-7ec8480e7487"))
     ]
-
-
-@patch("orggatekeeper.main.fetch_org_uuid")
-@patch("orggatekeeper.main.MOAMQPSystem")
-@patch("orggatekeeper.calculate.MORouter")
-async def test_lifespan(
-    mo_router: MagicMock,
-    mo_amqpsystem: MagicMock,
-    mock_fetch_org_uuid: MagicMock,
-    fastapi_app: FastAPI,
-) -> None:
-    """Test that our lifespan events are handled as expected."""
-    amqp_system = MagicMock()
-    amqp_system.start = AsyncMock()
-    amqp_system.stop = AsyncMock()
-
-    mo_amqpsystem.return_value = amqp_system
-    mock_fetch_org_uuid.return_value = ORG_UUID
-
-    router = MagicMock()
-    mo_router.return_value = router
-
-    assert not amqp_system.mock_calls
-
-    # Fire startup event on entry, and shutdown on exit
-    async with LifespanManager(fastapi_app):
-        assert len(router.mock_calls) == 0
-
-        # Clean mock to only capture shutdown changes
-        amqp_system.reset_mock()
-
-
-async def test_liveness_endpoint(test_client: TestClient) -> None:
-    """Test the liveness endpoint on our app."""
-    response = test_client.get("/health/live")
-    assert response.status_code == 204
-
-
-@pytest.mark.parametrize(
-    "amqp_ok,gql_ok,model_ok,expected",
-    [
-        (True, True, True, 204),
-        (False, True, True, 503),
-        (True, False, True, 503),
-        (True, True, False, 503),
-        (True, False, False, 503),
-        (False, True, False, 503),
-        (False, False, True, 503),
-        (False, False, False, 503),
-    ],
-)
-@patch("orggatekeeper.main.construct_context")
-async def test_readiness_endpoint(
-    construct_context: MagicMock,
-    test_client_builder: Callable[..., TestClient],
-    amqp_ok: bool,
-    gql_ok: bool,
-    model_ok: bool,
-    expected: int,
-) -> None:
-    """Test the readiness endpoint handles errors."""
-    gql_client = AsyncMock()
-    if gql_ok:
-        gql_client.execute.return_value = {
-            "org": {"uuid": "35304fa6-ff84-4ea4-aac9-a285995ab45b"}
-        }
-    else:
-        gql_client.execute.return_value = {
-            "errors": [{"message": "Something went wrong"}]
-        }
-
-    model_client_response = MagicMock()
-    if model_ok:
-        model_client_response.json.return_value = [
-            {"uuid": "35304fa6-ff84-4ea4-aac9-a285995ab45b"}
-        ]
-    else:
-        model_client_response.json.return_value = "BOOM"
-    model_client = AsyncMock()
-    model_client.async_httpx_client.get.return_value = model_client_response
-
-    amqp_system = MagicMock()
-    amqp_system.healthcheck.return_value = amqp_ok
-
-    construct_context.return_value = {
-        "legacy_graphql_session": gql_client,
-        "legacy_model_client": model_client,
-        "amqp_system": amqp_system,
-    }
-    test_client = test_client_builder()
-
-    response = test_client.get("/health/ready")
-    assert response.status_code == expected
-
-    assert len(gql_client.execute.mock_calls) == 1
-    print(model_client.mock_calls)
-    assert model_client.mock_calls == [
-        call.async_httpx_client.get("/service/o/"),
-        call.async_httpx_client.get().json(),
-    ]
-    assert amqp_system.mock_calls == [call.healthcheck()]
-
-
-@pytest.mark.parametrize(
-    "amqp_ok,gql_ok,model_ok,expected",
-    [
-        (True, True, True, 204),
-        (False, True, True, 503),
-        (True, False, True, 503),
-        (True, True, False, 503),
-        (True, False, False, 503),
-        (False, True, False, 503),
-        (False, False, True, 503),
-        (False, False, False, 503),
-    ],
-)
-@patch("orggatekeeper.main.construct_context")
-async def test_readiness_endpoint_exception(
-    construct_context: MagicMock,
-    test_client_builder: Callable[..., TestClient],
-    amqp_ok: bool,
-    gql_ok: bool,
-    model_ok: bool,
-    expected: int,
-) -> None:
-    """Test the readiness endpoint handled exceptions nicely."""
-    gql_client = AsyncMock()
-    if gql_ok:
-        gql_client.execute.return_value = {
-            "org": {"uuid": "35304fa6-ff84-4ea4-aac9-a285995ab45b"}
-        }
-    else:
-        gql_client.execute.side_effect = ValueError("BOOM")
-
-    model_client_response = MagicMock()
-    if model_ok:
-        model_client_response.json.return_value = [
-            {"uuid": "35304fa6-ff84-4ea4-aac9-a285995ab45b"}
-        ]
-    else:
-        model_client_response.json.side_effect = ValueError("BOOM")
-    model_client = AsyncMock()
-    model_client.async_httpx_client.get.return_value = model_client_response
-
-    amqp_system = MagicMock()
-    if amqp_ok:
-        amqp_system.healthcheck.return_value = True
-    else:
-        amqp_system.healthcheck.side_effect = ValueError("BOOM")
-
-    construct_context.return_value = {
-        "legacy_graphql_session": gql_client,
-        "legacy_model_client": model_client,
-        "amqp_system": amqp_system,
-    }
-    test_client = test_client_builder()
-
-    response = test_client.get("/health/ready")
-    assert response.status_code == expected
-
-
-@patch("orggatekeeper.main.PersistentGraphQLClient")
-def test_gql_client_created_with_timeout(
-    gql_client: MagicMock,
-    set_settings: Callable[..., Settings],
-) -> None:
-    """Test that PersistentGraphQLClient is called with timeout setting"""
-
-    # Arrange
-    settings = set_settings(fastramqpi={"graphql_timeout": 15})
-
-    # Act
-    construct_clients(settings)
-
-    # Assert
-    assert gql_client.call_args.kwargs["httpx_client_kwargs"]["timeout"] == 15
-    assert gql_client.call_args.kwargs["execute_timeout"] == 15
 
 
 @patch("orggatekeeper.api.update_line_management", return_value=AsyncMock())
