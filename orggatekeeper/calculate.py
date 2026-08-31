80f36b98f0eb1327dc0fa2212e7ed5772b60773b
Lorenzo Panieri <lp@magenta.dk>
2026-08-31 11:55:06 +0200

    refactor: [#70974] re-write shim to use GQLClient instead of ModelClient
    This commit is the one that actually decouples this integration from
    ModelClient, and thus the service API.

--- orggatekeeper/calculate.py
+++ orggatekeeper/calculate.py
@@ -411,13 +411,10 @@ async def update_line_management(
         uuid=uuid,
         new_org_unit_hierarchy=new_org_unit_hierarchy,
     )
-    logger.debug("Sending ModelClient edit request", org_unit=org_unit)
+    logger.debug("Sending GraphQL update request", org_unit=org_unit)
 
-    # FIXME(#71355): this can fail on occasion due to a bug in the service API.
-    #   I didn't fix it because the service API is gonna be deprecated soon
-    #   and the integration will just run again at a later point if it fails
-    response = await model_client_shim.edit(legacy_model_client, org_unit)
-    logger.debug("ModelClient response", response=response)
+    response = await model_client_shim.edit(graphql_client, org_unit)
+    logger.debug("GraphQL update response", response=response)
     if org_unit.parent is not None:
         # Check if parent org_unit needs to be updated.
         await update_line_management(
